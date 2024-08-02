12043af606d347d49375f2e063b318cb53e13ce2
tokyoboi0711 <azmididul@gmail.com>
2024-08-02 16:05:30 +0700

    Perubahan total (gallery1)
    penambahan tailwindcss, penambahan fitur js MODAL penambahan font,  penambahan grid

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./index.html", "./gallery1.html"],
+	theme: {
+		fontFamily: {
+			'lora': ['Lora'],
+			'poppins' : ['Poppins']
+		},
+		extend: {
+		},
+	},
+	plugins: [],
+};
